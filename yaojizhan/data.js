961efe111987c8b2995405bd83d3e8f23bf3b1d6
Devin Chen <d3vin.chen@gmail.com>
2015-12-23 17:31:38 +0800

    Add missing data

---
+++ yaojizhan/data.js
@@ -0,0 +1,95 @@
+var DATA = [];
+
+/*
+ * .---------------------. .---------------------. .---------------------.
+ * |                     | |                     | |                     |
+ * |                     | |                     | |                     |
+ * |       MAP (1)       | |       MAP (2)       | |       MAP (3)       |
+ * |                     | |                     | |                     |
+ * |                     | |                     | |                     |
+ * `---------------------` `---------------------` `---------------------`
+**/
+
+// MAP (1)
+
+DATA[0] = [];
+DATA[0][0] = {
+  "link": "../hanlinjinggongzhutiting/index.html",
+  "map": [155,270,230,170,380,190,395,270,360,355,180,355]
+};
+DATA[0][1] = {
+  "link": "../changfengjindinyiting/index.html",
+  "map": [15,485,80,385,303,370,330,457,290,556,50,555]
+};
+DATA[0][2] = {
+  "link": "../zhoukouyaoji/index.html",
+  "map": [605,195,785,295]
+};
+DATA[0][3] = {
+  "link": "../hangzhouyongchuangzhinengshebei/index.html",
+  "map": [590,305,790,420]
+};
+DATA[0][4] = {
+  "link": "../hunanqianshanzhiyaojixie/index.html",
+  "map": [580,440,820,590]
+};
+DATA[0][5] = {
+  "link": "../dongfulongzongheting/index.html",
+  "map": [870,300,930,490,1340,490,1220,300,1060,250]
+};
+
+// MAP (2)
+
+DATA[1] = [];
+DATA[1][0] = {
+  "link": "../jiangsujufeng/index.html",
+  "map": [155,270,230,170,380,190,395,270,360,355,180,355]
+};
+DATA[1][1] = {
+  "link": "../liaoningxiangan/index.html",
+  "map": [15,485,80,385,303,370,330,457,290,556,50,555]
+};
+DATA[1][2] = {
+  "link": "../wenzhoujiade/index.html",
+  "map": [605,195,785,295]
+};
+DATA[1][3] = {
+  "link": "../wenzhoujiade/index.html",
+  "map": [590,305,790,420]
+};
+DATA[1][4] = {
+  "link": "../wenzhoujiade/index.html",
+  "map": [580,440,820,590]
+};
+DATA[1][5] = {
+  "link": "../shanghaihuadong/index.html",
+  "map": [870,300,930,490,1340,490,1220,300,1060,250]
+};
+
+// MAP (3)
+
+DATA[2] = [];
+DATA[2][0] = {
+  "link": "../hanlinjinggongzhutiting/index.html",
+  "map": [155,270,230,170,380,190,395,270,360,355,180,355]
+};
+DATA[2][1] = {
+  "link": "../changfengjindinyiting/index.html",
+  "map": [15,485,80,385,303,370,330,457,290,556,50,555]
+};
+DATA[2][2] = {
+  "link": "",
+  "map": [605,195,785,295]
+};
+DATA[2][3] = {
+  "link": "",
+  "map": [590,305,790,420]
+};
+DATA[2][4] = {
+  "link": "",
+  "map": [580,440,820,590]
+};
+DATA[2][5] = {
+  "link": "../dongfulongzongheting/index.html",
+  "map": [870,300,930,490,1340,490,1220,300,1060,250]
+};
