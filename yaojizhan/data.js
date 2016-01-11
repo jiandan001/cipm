7e704660891da8203bb50dbcbea3648cef0cbbaf
Devin Chen <d3vin.chen@gmail.com>
2016-01-11 16:57:44 +0800

    Remove keywords

--- yaojizhan/data.js
+++ yaojizhan/data.js
@@ -16,38 +16,32 @@ DATA[0] = [];
 DATA[0][0] = {
   "link": "../hanlinjinggongzhutiting/index.html",
   "map": [65,71,144,131],
-  "name": "北京翰林航宇科技发展股份有限公司",
-  "keywords": "北京翰林航宇 翰林航宇"
+  "name": "北京翰林航宇科技发展股份有限公司"
 };
 DATA[0][1] = {
   "link": "../changfengjindinyiting/index.html",
   "map": [17,145,119,202],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[0][2] = {
   "link": "../zhoukouyaoji/index.html",
   "map": [229,73,294,109],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[0][3] = {
   "link": "../hangzhouyongchuangzhinengshebei/index.html",
   "map": [227,117,300,156],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[0][4] = {
   "link": "../hunanqianshanzhiyaojixie/index.html",
   "map": [219,169,313,231],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[0][5] = {
   "link": "../dongfulongzongheting/index.html",
   "map": [335,95,482,183],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 
 // MAP (2)
@@ -56,38 +50,32 @@ DATA[1] = [];
 DATA[1][0] = {
   "link": "../jiangsujufeng/index.html",
   "map": [80,71,172,125],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[1][1] = {
   "link": "../liaoningxiangan/index.html",
   "map": [16,134,169,214],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[1][2] = {
   "link": "../chutiankeji/index.html",
   "map": [218,67,307,125],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[1][3] = {
   "link": "../wenzhoujiade/index.html",
   "map": [212,132,347,214],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[1][4] = {
   "link": "../jiangyinbinjiang/index.html",
   "map": [354,69,472,144],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[1][5] = {
   "link": "../shanghaihuadong/index.html",
   "map": [385,153,493,212],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 
 // MAP (3)
@@ -96,36 +84,30 @@ DATA[2] = [];
 DATA[2][0] = {
   "link": "../hanlinjinggongzhutiting/index.html",
   "map": [155,270,230,170,380,190,395,270,360,355,180,355],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[2][1] = {
   "link": "../changfengjindinyiting/index.html",
   "map": [15,485,80,385,303,370,330,457,290,556,50,555],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[2][2] = {
   "link": "",
   "map": [605,195,785,295],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[2][3] = {
   "link": "",
   "map": [590,305,790,420],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[2][4] = {
   "link": "",
   "map": [580,440,820,590],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
 DATA[2][5] = {
   "link": "../dongfulongzongheting/index.html",
   "map": [870,300,930,490,1340,490,1220,300,1060,250],
-  "name": "",
-  "keywords": ""
+  "name": ""
 };
