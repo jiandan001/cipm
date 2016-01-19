ca5bc8e66c4cf039eb5100739ad287b25269b998
Devin Chen <d3vin.chen@gmail.com>
2016-01-19 13:32:50 +0800

    A switch to open new window.

--- yaojizhan/data.js
+++ yaojizhan/data.js
@@ -16,32 +16,38 @@ DATA[0] = [];
 DATA[0][0] = {
   "link": "../hanlinjinggongzhutiting/index.html",
   "map": [65,71,144,131],
-  "name": "北京翰林航宇科技发展股份有限公司"
+  "name": "北京翰林航宇科技发展股份有限公司",
+  "xinchuangkou": true
 };
 DATA[0][1] = {
   "link": "../changfengjindinyiting/index.html",
   "map": [17,145,119,202],
-  "name": "北京长峰金鼎科技有限公司"
+  "name": "北京长峰金鼎科技有限公司",
+  "xinchuangkou": false
 };
 DATA[0][2] = {
   "link": "../zhoukouyaoji/index.html",
   "map": [229,73,294,109],
-  "name": "周口制药机械厂有限公司"
+  "name": "周口制药机械厂有限公司",
+  "xinchuangkou": false
 };
 DATA[0][3] = {
   "link": "../hangzhouyongchuangzhinengshebei/index.html",
   "map": [227,117,300,156],
-  "name": "杭州永创智能设备股份有限公司"
+  "name": "杭州永创智能设备股份有限公司",
+  "xinchuangkou": false
 };
 DATA[0][4] = {
   "link": "../hunanqianshanzhiyaojixie/index.html",
   "map": [219,169,313,231],
-  "name": "湖南千山制药机械股份有限公司"
+  "name": "湖南千山制药机械股份有限公司",
+  "xinchuangkou": false
 };
 DATA[0][5] = {
   "link": "../dongfulongzongheting/index.html",
   "map": [335,95,482,183],
-  "name": "上海东富龙科技股份有限公司"
+  "name": "上海东富龙科技股份有限公司",
+  "xinchuangkou": false
 };
 
 // MAP (2)
@@ -50,32 +56,38 @@ DATA[1] = [];
 DATA[1][0] = {
   "link": "../jiangsujufeng/index.html",
   "map": [80,71,172,125],
-  "name": "江苏炬峰机械有限公司"
+  "name": "江苏炬峰机械有限公司",
+  "xinchuangkou": false
 };
 DATA[1][1] = {
   "link": "../liaoningxiangan/index.html",
   "map": [16,134,169,214],
-  "name": "辽宁祥安制药机械有限公司"
+  "name": "辽宁祥安制药机械有限公司",
+  "xinchuangkou": false
 };
 DATA[1][2] = {
   "link": "../chutiankeji/index.html",
   "map": [218,67,307,125],
-  "name": "楚天科技股份有限公司"
+  "name": "楚天科技股份有限公司",
+  "xinchuangkou": false
 };
 DATA[1][3] = {
   "link": "../wenzhoujiade/index.html",
   "map": [212,132,347,214],
-  "name": "温州佳德包装有限公司"
+  "name": "温州佳德包装有限公司",
+  "xinchuangkou": false
 };
 DATA[1][4] = {
   "link": "../jiangyinbinjiang/index.html",
   "map": [354,69,472,144],
-  "name": "江阴滨江医疗设备有限公司"
+  "name": "江阴滨江医疗设备有限公司",
+  "xinchuangkou": false
 };
 DATA[1][5] = {
   "link": "../shanghaihuadong/index.html",
   "map": [385,153,493,212],
-  "name": "上海华东制药机械有限公司"
+  "name": "上海华东制药机械有限公司",
+  "xinchuangkou": false
 };
 
 // MAP (3)
@@ -84,30 +96,36 @@ DATA[2] = [];
 DATA[2][0] = {
   "link": "../hanlinjinggongzhutiting/index.html",
   "map": [155,270,230,170,380,190,395,270,360,355,180,355],
-  "name": ""
+  "name": "",
+  "xinchuangkou": false
 };
 DATA[2][1] = {
   "link": "../changfengjindinyiting/index.html",
   "map": [15,485,80,385,303,370,330,457,290,556,50,555],
-  "name": ""
+  "name": "",
+  "xinchuangkou": false
 };
 DATA[2][2] = {
   "link": "",
   "map": [605,195,785,295],
-  "name": ""
+  "name": "",
+  "xinchuangkou": false
 };
 DATA[2][3] = {
   "link": "",
   "map": [590,305,790,420],
-  "name": ""
+  "name": "",
+  "xinchuangkou": false
 };
 DATA[2][4] = {
   "link": "",
   "map": [580,440,820,590],
-  "name": ""
+  "name": "",
+  "xinchuangkou": false
 };
 DATA[2][5] = {
   "link": "../dongfulongzongheting/index.html",
   "map": [870,300,930,490,1340,490,1220,300,1060,250],
-  "name": ""
+  "name": "",
+  "xinchuangkou": false
 };
